609008b3ee92d50e45c73c2a2f9db8265585c228
Ammar Ajmal <ammarajml@gmail.com>
2023-07-13 16:31:28 +0900

    -first

---
+++ src/components/About.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+class About extends Component {
+    render() {
+        return (
+            <div>
+                <h1>About!</h1>
+                <p>This is a simple React app controls and monitors ROS-enabled system through a Web Interface.</p>
+            </div>
+        );
+    }
+}
+
+export default About;
