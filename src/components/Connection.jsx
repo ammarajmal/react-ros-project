4c4c85d8b231eb2a3538eb225aeee1cff5be24c7
Ammar Ajmal <ammarajml@gmail.com>
2023-07-29 23:12:22 +0900

    new

--- src/components/Connection.jsx
+++ src/components/Connection.jsx
@@ -1,14 +1,57 @@
 import React, {Component} from "react";
-import ROSLIB from "roslib";
-import ROS2D from "ros2d";
+import { Alert } from "react-bootstrap";
+import Config from "../scripts/config.js";
 class Connection extends Component {
-    state = {};
+    state = {
+        connected: false,
+        ros: null
+    };
+    constructor(){
+        super();
+        this.initConnection();
+    }
     initConnection() {
-        let ros = new ROSLIB.Ros();
+        // this.setState({ros: new window.ROSLIB.Ros()})
+        this.state.ros = new window.ROSLIB.Ros();
+        console.log(this.state.ros);
+
+        this.state.ros.on("connection", () => {
+            console.log("Connection made!");
+            this.setState({connected: true});
+        });
+        this.state.ros.on("close", () => {
+            console.log("Connection closed.");
+            this.setState({connected: false});
+            // try to reconnect every 3 seconds
+            setTimeout(() => {
+                try{
+                    this.state.ros.connect("ws://"+Config.ROSBRIDGE_SERVER_IP+":"+Config.ROSBRIDGE_SERVER_PORT);
+                } catch (error) {
+                    console.log("Connection failed!");
+                    this.setState({connected: false});
+                }
+            }, Config.RECONNECTION_TIMEOUT);
+            
+        });
+
+        try{
+            this.state.ros.connect("ws://"+Config.ROSBRIDGE_SERVER_IP+":"+Config.ROSBRIDGE_SERVER_PORT);
+        } catch (error) {
+            console.log("Connection failed!");
+            this.setState({connected: false});
+        }
     }
 render() {
             return (
-                <h2>Connection</h2>
+                <div>
+                    {
+                        <Alert className="text-center m-3" variant={this.state.connected?"success":"danger"}>
+                             {this.state.connected?"Camera Connected":"Camera Disconnected"}
+                        </Alert>
+                    }
+                         
+                </div>
+
 
             );
     }
---
+++ src/scripts/config.js
@@ -0,0 +1,6 @@
+const Config = {
+    ROSBRIDGE_SERVER_IP: "0.0.0.0",
+    ROSBRIDGE_SERVER_PORT: 9090,
+    RECONNECTION_TIMEOUT: 2000
+};
+export default Config;
